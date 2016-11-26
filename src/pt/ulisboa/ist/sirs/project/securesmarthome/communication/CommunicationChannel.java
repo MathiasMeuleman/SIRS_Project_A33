90ce6f217cd2636bfd92c852277b6e2435699d8e
tlammers <torben-lammers@gmx.de>
2016-11-26 19:28:39 +0000

    classes for the communication between the smart home entities

---
+++ src/pt/ulisboa/ist/sirs/project/securesmarthome/communication/CommunicationChannel.java
@@ -0,0 +1,11 @@
+package pt.ulisboa.ist.sirs.project.securesmarthome.communication;
+
+/**
+ * Created by Alex Anders on 21/11/2016.
+ */
+public interface CommunicationChannel {
+
+    void sendMessage(String string);
+
+    String receiveMessage();
+}
---
+++ src/pt/ulisboa/ist/sirs/project/securesmarthome/communication/SimpleChannel.java
@@ -0,0 +1,21 @@
+package pt.ulisboa.ist.sirs.project.securesmarthome.communication;
+
+/**
+ * Created by Alex Anders on 21/11/2016.
+ */
+public class SimpleChannel implements CommunicationChannel {
+
+    @Override
+    public void sendMessage(String string) {
+        message = new String(string);
+    }
+
+    @Override
+    public String receiveMessage() {
+        if (message == null)
+            message = new String("");
+        return message;
+    }
+
+    private String message;
+}
