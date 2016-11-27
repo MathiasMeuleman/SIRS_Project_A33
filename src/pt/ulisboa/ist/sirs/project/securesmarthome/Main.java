12c6b931d31f01d6bc536260b62c3fda5d29b7dc
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-11-27 16:42:09 +0000

    Create KeyStore

--- src/pt/ulisboa/ist/sirs/project/securesmarthome/Main.java
+++ src/pt/ulisboa/ist/sirs/project/securesmarthome/Main.java
@@ -11,34 +11,33 @@ import javax.crypto.SecretKey;
  */
 public class Main {
     public static void main(String argv[]) throws Exception {
-//        AESKeyGenerator gen = new AESKeyGenerator();
-//        SecretKey key = gen.generateKey();
-//        System.out.println(key.getEncoded());
-//        GenericKeyStore store = new GenericKeyStore();
-//        store.storeKey(key, "key1");
-//        SecretKey key2 = store.loadKey("key1");
-//        System.out.println(key2.getEncoded());
+        AESKeyGenerator gen = new AESKeyGenerator();
+        SecretKey key = gen.generateKey();
+        GenericKeyStore store = new GenericKeyStore();
+        store.storeKey(key, "key1");
+        SecretKey key2 = store.loadKey("key1");
+        System.out.println("(key1 == key2) = " + key.equals(key2));
 
-        try {
-            String mode = "USE_SKIP_DH_PARAMS";
-
-            DHKeyAgreement2 keyAgree = new DHKeyAgreement2();
-
-            if (argv.length > 1) {
-                keyAgree.usage();
-                throw new Exception("Wrong number of command options");
-            } else if (argv.length == 1) {
-                if (!(argv[0].equals("-gen"))) {
-                    keyAgree.usage();
-                    throw new Exception("Unrecognized flag: " + argv[0]);
-                }
-                mode = "GENERATE_DH_PARAMS";
-            }
-
-            keyAgree.run(mode);
-        } catch (Exception e) {
-            System.err.println("Error: " + e);
-            System.exit(1);
-        }
+//        try {
+//            String mode = "USE_SKIP_DH_PARAMS";
+//
+//            DHKeyAgreement2 keyAgree = new DHKeyAgreement2();
+//
+//            if (argv.length > 1) {
+//                keyAgree.usage();
+//                throw new Exception("Wrong number of command options");
+//            } else if (argv.length == 1) {
+//                if (!(argv[0].equals("-gen"))) {
+//                    keyAgree.usage();
+//                    throw new Exception("Unrecognized flag: " + argv[0]);
+//                }
+//                mode = "GENERATE_DH_PARAMS";
+//            }
+//
+//            keyAgree.run(mode);
+//        } catch (Exception e) {
+//            System.err.println("Error: " + e);
+//            System.exit(1);
+//        }
     }
 }
