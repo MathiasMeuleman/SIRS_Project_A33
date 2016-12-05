ce9f9a177c4851d760cde409eaace7b28642ad91
tlammers <torben-lammers@gmx.de>
2016-12-05 23:04:23 +0000

    Change LightBulb class LightBulb has now simulation method and can be swtiched on and off. If swtiched the LightBuld pushes the current state.

--- src/pt/ulisboa/ist/sirs/project/securesmarthome/smarthomedevice/LightBulb.java
+++ src/pt/ulisboa/ist/sirs/project/securesmarthome/smarthomedevice/LightBulb.java
@@ -1,15 +1,60 @@
 package pt.ulisboa.ist.sirs.project.securesmarthome.smarthomedevice;
 
+import java.net.SocketException;
+
 /**
  * Created by Mathias on 2016-12-03.
  */
 public class LightBulb {
 
-    /////////////////////////////////////////////////
-    // Will serve as command-receiving example SHD //
-    /////////////////////////////////////////////////
+    private SHDSecurity security;
+    private LightState lightState;
+
+    public LightBulb(SHDSecurity security) {
+        this.security = security;
+        lightState = LightState.OFF;
+    }
 
     public void run() {
+        security.connectToDevice();
+        lightSim();
+    }
 
+    private void lightSim()
+    {
+        while (true)
+        {
+            try {
+                String command = security.receive().toString();
+                System.out.println("command received: " + command);
+                toggleLight();
+                // acknowledge the process
+                security.send(lightState.toString().getBytes());
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void toggleLight()
+    {
+        if (lightState == LightState.ON)
+            turnLightOff();
+        if (lightState == LightState.OFF)
+            turnLightOn();
+    }
+
+    private void turnLightOff()
+    {
+        lightState = LightState.OFF;
+    }
+
+    private void turnLightOn()
+    {
+        lightState = LightState.ON;
+    }
+
+    private enum LightState{
+        ON,OFF
     }
 }
