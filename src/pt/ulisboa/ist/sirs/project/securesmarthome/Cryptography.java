543673d50ff529174aa213cf6599d0927cfe8408
Mathias Meuleman <m.meuleman@student.tudelft.nl>
2016-12-05 18:13:21 +0000

    Use CBC for in Cryptography

--- src/pt/ulisboa/ist/sirs/project/securesmarthome/Cryptography.java
+++ src/pt/ulisboa/ist/sirs/project/securesmarthome/Cryptography.java
@@ -6,137 +6,65 @@ package pt.ulisboa.ist.sirs.project.securesmarthome;
 import pt.ulisboa.ist.sirs.project.securesmarthome.keymanagement.AESSecretKeyGenerator;
 
 import java.security.*;
-import java.io.*;
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 
 public class Cryptography {
 
     private final static String ALGO = "AES";
-    private final static String MODE = "ECB";
+    private final static String MODE = "CBC";
     private final static String PADDING = "PKCS5Padding";
+    private static byte[] ivVector = {(byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00,
+            (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, };
 
-
-public static void main(String args[]) {
-    try {
-        byte[] plaintext = {(byte) 0x30, (byte) 0x31, (byte) 0x32};
-        AESSecretKeyGenerator keyGenerator = new AESSecretKeyGenerator();
-        SecretKey key = keyGenerator.generateKey();
-        System.out.println("plaintext: " + new String(plaintext, "UTF8"));
-        byte[] cryptogramm = encrypt(plaintext, key);
-        System.out.println("cryptogramm: " + new String(cryptogramm, "UTF8"));
-        byte[] recoveredPlaintext = decrypt(cryptogramm, key);
-        System.out.println("recoveredPlaintext: " + new String(recoveredPlaintext, "UTF8"));
-        if (!java.util.Arrays.equals(plaintext, recoveredPlaintext))
-            System.out.println("DES in CBC mode recovered text is " +
-                    "different from cleartext");
-        else
-            System.out.println("DES in ECB mode recovered text is " +
-                    "same as cleartext");
-    } catch (Exception e) {
-        System.out.println("Exception: " + e);
+    public static void main(String args[]) {
+        try {
+            byte[] plaintext = {(byte) 0x30, (byte) 0x31, (byte) 0x32};
+            AESSecretKeyGenerator keyGenerator = new AESSecretKeyGenerator();
+            SecretKey key = keyGenerator.generateKey();
+            System.out.println("plaintext: " + new String(plaintext, "UTF8"));
+            byte[] cryptogramm = encrypt(plaintext, key);
+            System.out.println("cryptogramm: " + new String(cryptogramm, "UTF8"));
+            byte[] recoveredPlaintext = decrypt(cryptogramm, key);
+            System.out.println("recoveredPlaintext: " + new String(recoveredPlaintext, "UTF8"));
+            if (!java.util.Arrays.equals(plaintext, recoveredPlaintext))
+                System.out.println("DES in CBC mode recovered text is " +
+                        "different from cleartext");
+            else
+                System.out.println("DES in ECB mode recovered text is " +
+                        "same as cleartext");
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
     }
-}
 
     public static byte[] encrypt(byte[] plaintext, Key key) {
-        byte[] ciphertext = null;
-
+        byte[] ciphertext;
         try {
-//            System.out.println("Start encryption ...");
-
-    /* Get Message to be sent */
-//            System.out.println("Message to encrypt: " + plaintext);
-
-    /* Create the Cipher and init it with the secret key */
             Cipher c = Cipher.getInstance(ALGO+"/"+MODE+"/"+PADDING);
-            c.init(Cipher.ENCRYPT_MODE, key);
+            c.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(ivVector));
             ciphertext = c.doFinal(plaintext);
-
+            return ciphertext;
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return ciphertext;
+        return null;
     }
 
     public static byte[] decrypt(byte[] ciphertext, Key key) {
-        byte[] plaintext = null;
+        byte[] plaintext;
         try {
-
-    /* Create a Cipher object and initialize it with the secret key */
             Cipher c = Cipher.getInstance(ALGO+"/"+MODE+"/"+PADDING);
-            c.init(Cipher.DECRYPT_MODE,key);
-
-    /* Update and decrypt */
+            c.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ivVector));
             plaintext = c.doFinal(ciphertext);
             return plaintext;
-
         } catch (BadPaddingException e) {
             System.out.println("Wrong key used for decryption!");
-            return null;
         }
         catch (Exception e)
         {
             e.printStackTrace();
-            return null;
         }
-    }
-
-    private static String getMessage() {
-        String dest = "localhost:port";
-        String message = "Message I want to send";
-        final String rc = System.getProperty("line.separator");
-        StringBuffer buf = new StringBuffer();
-        buf.append(dest);
-        buf.append(rc);
-        buf.append(message);
-        return buf.toString();
-    }
-
-    /* Writing encrypted text in a file */
-    private static void save(byte[] buf, String file) throws IOException {
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.write(buf);
-        fos.close();
-    }
-
-    /* Reading from a file to decrypt */
-    private static byte[] load(String file) throws FileNotFoundException, IOException {
-        FileInputStream fis = new FileInputStream(file);
-        byte[] buf = new byte[fis.available()];
-        fis.read(buf);
-        fis.close();
-        return buf;
+        return null;
     }
 }
-
-/*
-In case we want to use CBC mode we have some example below
- */
-//        /*
-//         * Bob encrypts, using DES in CBC mode
-//         */
-//        bobCipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
-//        bobCipher.init(Cipher.ENCRYPT_MODE, bobDesKey);
-//
-//        cleartext = "This is just an example".getBytes();
-//        ciphertext = bobCipher.doFinal(cleartext);
-//        // Retrieve the parameter that was used, and transfer it to Alice in
-//        // encoded format
-//        byte[] encodedParams = bobCipher.getParameters().getEncoded();
-//
-//    /*
-//     * Alice decrypts, using DES in CBC mode
-//     */
-//        // Instantiate AlgorithmParameters object from parameter encoding
-//        // obtained from Bob
-//        AlgorithmParameters params = AlgorithmParameters.getInstance("DES");
-//        params.init(encodedParams);
-//        aliceCipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
-//        aliceCipher.init(Cipher.DECRYPT_MODE, aliceDesKey, params);
-//        recovered = aliceCipher.doFinal(ciphertext);
-//
-//        if (!java.util.Arrays.equals(cleartext, recovered))
-//            throw new Exception("DES in CBC mode recovered text is " +
-//                    "different from cleartext");
-//        System.out.println("DES in CBC mode recovered text is " +
-//                "same as cleartext");
-//    }
